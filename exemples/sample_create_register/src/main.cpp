08e75c3f654cead11e5c28a3fc5c7eff1a38bd53
jbruel <jonathan.bruel@epitech.eu>
2019-05-14 15:40:41 +0200

    Finish exemple setting

--- exemples/sample_create_register/src/main.cpp
+++ exemples/sample_create_register/src/main.cpp
@@ -1,12 +1,22 @@
-#include <iostream>
-#include "jbr/library.h"
+//!
+//! @file main.cpp
+//! @author jbruel
+//! @date 29/04/19
+//!
+
 #include "jbr/reg/HelloWorld.hpp"
+#include <iostream>
+#include <thread>
+#include <chrono>
 
 int main()
 {
     jbr::reg::HelloWorld    hw;
 
     std::cout << hw.hello() << std::endl;
-    while (true);
+    std::cout << hw.world() << std::endl;
+    std::cout << hw.generateRandomNumber() << std::endl;
+    std::cout << hw.headerFunction(3) << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(4000));
     return 0;
 }
