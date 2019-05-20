22d51a8031d0b4ef54fffa2c2ad2e51d935b5ed3
jbruel <jonathan.bruel@outlook.fr>
2019-05-20 11:36:37 +0200

    start absolute path extractor

---
+++ include/jbr/reg/GetAbsolutePath.hpp
@@ -0,0 +1,41 @@
+//!
+//! @file GetFullPath.hpp
+//! @author jbruel
+//! @date 20/05/19
+//!
+
+#ifndef JBR_CREGISTER_GETABSOLUTEPATH_HPP
+# define JBR_CREGISTER_GETABSOLUTEPATH_HPP
+
+# include <string>
+# include "jbr/reg/exception.hpp"
+# include <filesystem>
+
+//!
+//! @namespace jbr
+//!
+namespace jbr
+{
+//!
+//! @namespace reg
+//!
+namespace reg
+{
+
+    //!
+    //! @static
+    //! @brief Extract absolute path from relative path.
+    //! @param relativePath Relative reference path.
+    //! @return Absolute path generate from relative input path.
+    //!
+    static const std::string    getAbsolutePath(const std::string &relativePath)
+    {
+        if (relativePath.empty())
+            throw jbr::reg::exception("Relative input string must not be empty.");
+        return (std::filesystem::absolute(relativePath));
+    }
+
+}
+}
+
+#endif //JBR_CREGISTER_GETABSOLUTEPATH_HPP
