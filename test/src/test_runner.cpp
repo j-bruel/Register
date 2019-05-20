8ee7102cdff822fa6ae4f8f46d7a438676dba7be
jbruel <jonathan.bruel@outlook.fr>
2019-05-20 11:37:37 +0200

    Remove warning from doctest lib

--- test/src/test_runner.cpp
+++ test/src/test_runner.cpp
@@ -1,2 +1,6 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "doctest.h"
+
+# pragma GCC diagnostic push
+# pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#  include "doctest.h"
+# pragma GCC diagnostic pop
