7758626cae12d6271412dccb465c1f36788a1c9f
jbruel <jonathan.bruel@outlook.fr>
2019-06-05 16:09:30 +0200

    Update exemple exe

--- exemples/sample_create_register/src/main.cpp
+++ exemples/sample_create_register/src/main.cpp
@@ -4,19 +4,17 @@
 //! @date 29/04/19
 //!
 
-#include "jbr/reg/HelloWorld.hpp"
+#include <jbr/Register.hpp>
 #include <iostream>
 #include <thread>
 #include <chrono>
 
 int main()
 {
-    jbr::reg::HelloWorld    hw;
+    jbr::Register   reg;
 
-    std::cout << hw.hello() << std::endl;
-    std::cout << hw.world() << std::endl;
-    std::cout << hw.generateRandomNumber() << std::endl;
-    std::cout << hw.headerFunction(3) << std::endl;
+    reg.create("./sample.reg");
+    std::cout << "Is my register existing ? " << reg.exist("./sample.reg") << std::endl;
     std::this_thread::sleep_for(std::chrono::milliseconds(4000));
-    return 0;
+    return (EXIT_SUCCESS);
 }
