f66633e947f044debca666b1bfae3e122cb11519
jbruel <jonathan.bruel@outlook.fr>
2019-06-05 16:08:56 +0200

    Adding test for exist function

---
+++ test/src/register_exist_test.cpp
@@ -0,0 +1,31 @@
+//!
+//! @file exist_create.cpp
+//! @author jbruel
+//! @date 05/06/19
+//!
+
+#include <jbr/Register.hpp>
+#include <doctest.h>
+
+TEST_CASE("Register::exist")
+{
+    jbr::Register   mRegister;
+
+    SUBCASE("empty input path")
+    {
+        CHECK(!mRegister.exist(""));
+    }
+
+    SUBCASE("exist")
+    {
+        mRegister.create("./exist");
+        CHECK(mRegister.exist("./exist"));
+        std::filesystem::remove("./exist");
+    }
+
+    SUBCASE("not exist")
+    {
+        CHECK(!mRegister.exist("./not_exist"));
+    }
+
+}
