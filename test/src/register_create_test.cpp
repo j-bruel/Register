621c0e763a8e1fb1336e7eb478293357cc473f10
jbruel <jonathan.bruel@outlook.fr>
2019-06-05 14:32:28 +0200

    Add tests for create functions

--- test/src/register_create_test.cpp
+++ test/src/register_create_test.cpp
@@ -5,6 +5,7 @@
 //!
 
 #include <jbr/Register.hpp>
+#include <jbr/reg/exception.hpp>
 #include <doctest.h>
 #include <iostream>
 #include <string>
@@ -15,8 +16,64 @@ TEST_CASE("Register::create")
 
     SUBCASE("empty input path")
     {
-        CHECK_THROWS(mRegister.create("toto.txt"));
-        // need to destroy or remove the created file ...
+        std::string msg;
+
+        try {
+            mRegister.create("");
+        }
+        catch (jbr::reg::exception &e) {
+            msg = e.what();
+        }
+        CHECK(msg == "To create a register the path must not be empty.");
+    }
+
+    SUBCASE("create one")
+    {
+        CHECK_NOTHROW(mRegister.create("./ut_create_one_register"));
+        CHECK(std::filesystem::exists("./ut_create_one_register"));
+        std::filesystem::remove("./ut_create_one_register");
+    }
+
+    SUBCASE("create few")
+    {
+        CHECK_NOTHROW(mRegister.create("./ut_create_1"));
+        CHECK_NOTHROW(mRegister.create("../ut_create_2"));
+        CHECK_NOTHROW(mRegister.create("ut_create_3"));
+        std::filesystem::create_directory("./nextDirectory");
+        CHECK_NOTHROW(mRegister.create("./nextDirectory/ut_create_4"));
+        CHECK(std::filesystem::exists("./ut_create_1"));
+        CHECK(std::filesystem::exists("../ut_create_2"));
+        CHECK(std::filesystem::exists("ut_create_3"));
+        CHECK(std::filesystem::exists("./nextDirectory/ut_create_4"));
+        std::filesystem::remove("./ut_create_1");
+        std::filesystem::remove("../ut_create_2");
+        std::filesystem::remove("ut_create_3");
+        std::filesystem::remove_all("./nextDirectory");
+    }
+
+    SUBCASE("already exist")
+    {
+        std::string msg;
+
+        CHECK_NOTHROW(mRegister.create("./ut_already_1"));
+        std::filesystem::create_directory("./ut_already_2");
+        try {
+            mRegister.create("./ut_already_1");
+        }
+        catch (jbr::reg::exception &e) {
+            msg = e.what();
+        }
+        CHECK(msg == "The register ./ut_already_1 already exist. You must remove it before create it or open it.");
+        msg.clear();
+        try {
+            mRegister.create("./ut_already_2");
+        }
+        catch (jbr::reg::exception &e) {
+            msg = e.what();
+        }
+        CHECK(msg == "The register ./ut_already_2 already exist. You must remove it before create it or open it.");
+        std::filesystem::remove_all("./ut_already_1");
+        std::filesystem::remove_all("./ut_already_2");
     }
 
 }
