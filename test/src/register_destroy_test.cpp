a83043fc7e56749b49a718cd6f350db275f66e8f
jbruel <jonathan.bruel@epitech.eu>
2019-07-02 17:14:19 +0200

    patch destroy testing

--- test/src/register_destroy_test.cpp
+++ test/src/register_destroy_test.cpp
@@ -32,41 +32,7 @@ TEST_CASE("Register::destroy")
         CHECK_NOTHROW(mRegister.destroy("./ut_destroy_one_register"));
         CHECK(!std::filesystem::exists("./ut_destroy_one_register"));
     }
-/*
-    SUBCASE("destroy few")
-    {
-        mRegister.create("./ut_destroy_1");
-        mRegister.create("../ut_destroy_2");
-        mRegister.create("ut_destroy_3");
-        std::filesystem::create_directory("./nextDirectory");
-        mRegister.create("./nextDirectory/ut_destroy_4");
-        CHECK_NOTHROW(mRegister.destroy("./ut_destroy_1"));
-        CHECK_NOTHROW(mRegister.destroy("../ut_destroy_2"));
-        CHECK_NOTHROW(mRegister.destroy("ut_destroy_3"));
-        CHECK_NOTHROW(mRegister.destroy("./nextDirectory/ut_destroy_4"));
-        CHECK(!std::filesystem::exists("./ut_create_1"));
-        CHECK(!std::filesystem::exists("../ut_create_2"));
-        CHECK(!std::filesystem::exists("ut_create_3"));
-        CHECK(!std::filesystem::exists("./nextDirectory/ut_create_4"));
-        std::filesystem::remove("./ut_destroy_1");
-        std::filesystem::remove("../ut_destroy_2");
-        std::filesystem::remove("ut_destroy_3");
-        std::filesystem::remove_all("./nextDirectory");
-    }
-
-    SUBCASE("not exist")
-    {
-        std::string msg;
-
-        try {
-            mRegister.destroy("./ut_not_exist");
-        }
-        catch (jbr::reg::exception &e) {
-            msg = e.what();
-        }
-        CHECK(msg == "Impossible to destroy a not existing register : ./ut_not_exist.");
-    }
-*/
+    
 }
 
 
