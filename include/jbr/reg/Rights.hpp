c3961b3db8490acc615638dcec95ae9415dd6437
jbruel <jonathan.bruel@epitech.eu>
2019-07-02 16:16:54 +0200

    Patch destroy source header

---
+++ include/jbr/reg/Rights.hpp
@@ -0,0 +1,8 @@
+//
+// Created by jonat on 02/07/2019.
+//
+
+#ifndef CREGISTER_RIGHTS_HPP
+#define CREGISTER_RIGHTS_HPP
+
+#endif //CREGISTER_RIGHTS_HPP
--- test/src/register_destroy_test.cpp
+++ test/src/register_destroy_test.cpp
@@ -8,39 +8,9 @@
 #include <jbr/reg/exception.hpp>
 #include <doctest.h>
 
-TEST_CASE("Register::destroyTestFuck")
+TEST_CASE("Register::destroy")
 {
     jbr::Register   mRegister;
-
-    SUBCASE("empty input path222")
-    {
-        std::string msg;
-
-        try {
-        mRegister.verify("");
-        }
-        catch (jbr::reg::exception &e) {
-        msg = e.what();
-        }
-        CHECK(msg == "To check if a register is corrupt the path must not be empty.");
-    }
-
-    SUBCASE("not existing22")
-    {
-        std::string msg;
-
-        try {
-        mRegister.verify("./ut_not_exist");
-        }
-        catch (jbr::reg::exception &e) {
-        msg = e.what();
-        }
-        CHECK(msg == "Impossible to check the corruption status of a not existing register : ./ut_not_exist.");
-    }
-
-    /**
-     * All usecase on validity function are done into the open function.
-     */
 
 }
 
