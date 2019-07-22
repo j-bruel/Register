76b7d98fde68a0ce55ee14c6bf1a40a4592df9df
jbruel <jonathan.bruel@outlook.fr>
2019-07-22 16:21:46 +0200

    Write unit test for operator variables

---
+++ test/src/reg/variable_operator_test.cpp
@@ -0,0 +1,50 @@
+//!
+//! @file variable_test.cpp
+//! @author jbruel
+//! @date 15/07/19
+//!
+
+#include <doctest.h>
+#include <jbr/Register.hpp>
+#include <jbr/reg/Variable.hpp>
+#include <jbr/reg/exception.hpp>
+
+TEST_CASE("Variable::operator")
+{
+
+    jbr::Register   mRegister;
+
+    SUBCASE("empty input path")
+    {
+        std::string msg;
+
+        try {
+            mRegister[""].get("none");
+        }
+        catch (jbr::reg::exception &e) {
+            msg = e.what();
+        }
+        CHECK(msg == "Register variable can not be build according a empty register path.");
+    }
+
+    SUBCASE("not existing register")
+    {
+        std::string msg;
+
+        try {
+            mRegister["./not_exist.reg"].get("none");
+        }
+        catch (jbr::reg::exception &e) {
+            msg = e.what();
+        }
+        CHECK(msg == "Register variable can not be build according a not existing register : ./not_exist.reg.");
+    }
+
+    SUBCASE("valid register")
+    {
+        mRegister.create("./valid.reg");
+        mRegister["./valid.reg"].get("none");
+        mRegister.destroy("./valid.reg");
+    }
+
+}
